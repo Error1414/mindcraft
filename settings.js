a7967f1d07946268373309c6fac1b9f908084ee7
Error1414 <richard.ingo2013@gmail.com>
2025-01-07 11:53:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,18 +11,20 @@ export default
     "mindserver_port": 8080,
     
     "profiles": [
-        "./andy.json",
-        // "./profiles/gpt.json",
-        // "./profiles/claude.json",
-        // "./profiles/gemini.json",
-        // "./profiles/llama.json",
-        // "./profiles/qwen.json",
-        // "./profiles/grok.json",
-        // "./profiles/deepseek.json",
+		"./profiles/gpt.json",  
+		"./profiles/gpt1.json",  
+		"./profiles/gpt2.json",  
+		"./profiles/gpt3.json",  
+		"./profiles/gpt4.json",  
+		"./profiles/gpt5.json",  
+		"./profiles/gpt6.json",  
+		"./profiles/gpt7.json",  
+		"./profiles/gpt8.json",  
+		"./profiles/gpt9.json",  
         
         // using more than 1 profile requires you to /msg each bot indivually
     ],
-    "load_memory": false, // load memory from previous session
+    "load_memory": true, // load memory from previous session
     "init_message": "Respond with hello world and your name", // sends to all on spawn
     "only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
     
@@ -30,11 +32,11 @@ export default
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
-    "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
+    "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
 
     "max_messages": 15, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
     "verbose_commands": true, // show full command syntax
-    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
+    "narrate_behavior": false, // chat simple automatic actions ('Picking up item!')
     "chat_bot_messages": true, // publicly chat messages to other bots
 }
